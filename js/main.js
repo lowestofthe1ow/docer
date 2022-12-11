f4553a0c9798b45ba07f29363f5e25f94dc76f44
Lorenz Marqueses <1owestoftheow@gmail.com>
2022-12-11 14:47:20 +0800

    Disable buttons

--- js/main.js
+++ js/main.js
@@ -23,22 +23,35 @@ $(document).ready(function() {
     try {
       const docRef = await setDoc(doc(db, "users", $("#email").val()), userObject);
       console.log("Document written with ID: ", $("#email").val());
+      alert("Registered successfully.")
+      $("#upload").prop("disabled", false);
     } catch (e) {
       console.error("Error adding document: ", e);
     }
   };
 
   async function getFromDB(type) {
+    let docSnap = await getDoc(doc(db, "users", logged_in_id));
     if (type == "teacher") {
       let querySnapshot = await getDocs(query(collection(db, "users"), where("type", "==", type)));
+      let partnerCounter = $("<p>", {
+        html: "You currently have " + String(docSnap.data().teachers.length) + " teacher(s):<br />" + docSnap.data().teachers.join("<br />")
+      })
+      $("#partnerCounter").append(partnerCounter);
       querySnapshot.forEach((doc) => {
         if (doc.data().students == null || doc.data().students.includes(logged_in_id) == false) {
-          createNewSearchResult(doc, type)
+          createNewSearchResult(doc, type, "#teachers", true)
+        }
+        else {
+
         }
       });
     }
     else {
-      let docSnap = await getDoc(doc(db, "users", logged_in_id));
+      let partnerCounter = $("<p>", {
+        html: "You currently have " + String(docSnap.data().students.length) + " student(s)<br />" + docSnap.data().students.join("<br />")
+      })
+      $("#partnerCounter").append(partnerCounter);
       if (docSnap.exists()) {
         let requestCounter = $("<p>", {
           html: "You have " + String(docSnap.data().requests.length) + " request(s)."
@@ -47,14 +60,14 @@ $(document).ready(function() {
         docSnap.data().requests.forEach(async function(doc_id) {
           let docReq = await getDoc(doc(db, "users", doc_id));
           if (docReq.exists()) {
-            createNewSearchResult(docReq, type);
+            createNewSearchResult(docReq, type, "#teachers", true);
           };
         });
       };
     };
   };
 
-  function createNewSearchResult(doc, type) {
+  function createNewSearchResult(doc, type, container, showButton) {
     let subheading = type == "student" ? doc.data().level : doc.data().specialty;
     let container_div = $("<div>", {
       id: doc.id,
@@ -72,19 +85,22 @@ $(document).ready(function() {
     let subheading_span = $("<span>", {
       html: doc.id + "<br />" + subheading
     })
-    let requestButton = $("<button>", {
-      html: type == "student" ? "Accept" : "Request",
-      id: doc.data().first,
-      class: type == "student" ? "acceptButton" : "requestButton"
-    })
 
-    $("#teachers").append(container_div);
-    container_div.append(requestButton);
+    $(container).append(container_div);
+
+    if (showButton == true) {
+      let requestButton = $("<button>", {
+        html: type == "student" ? "Accept" : "Request",
+        id: doc.data().first,
+        class: type == "student" ? "acceptButton" : "requestButton"
+      })
+      container_div.append(requestButton);
+    }
     container_div.append(avatar_image);
     container_div.append(text_container);
     text_container.append(name);
     text_container.append(subheading_span);
-    $("#teachers").append($("<br>"));
+    $(container).append($("<br>"));
   }
 
   async function verifyFromDB(id) {
@@ -94,6 +110,7 @@ $(document).ready(function() {
     if (docSnap.exists()) {
       if ($("#login_pass").val() != docSnap.data().pass) {
         alert("Incorrect password!")
+        $("#login_upload").prop("disabled", false);
       }
       else {
         logged_in_id = id;
@@ -121,12 +138,15 @@ $(document).ready(function() {
   }
 
   $("#login_upload").click(async function() {
+    $("#login_upload").prop("disabled", true);
     if (await matchFromDB($("#login_email").val()) == true) {
       verifyFromDB($("#login_email").val());
     }
   });
 
   $("#refresh").click(function() {
+    $("#requestCounter").html("");
+    $("#partnerCounter").html("");
     $("#teachers").html("");
     getFromDB(requestedRegisType);
   });
@@ -150,19 +170,21 @@ $(document).ready(function() {
       await setDoc(docRef, {requests: arrayRemove(target_email)}, {merge: true});
       await setDoc(docRef, {students: arrayUnion(target_email)}, {merge: true});
       alert("Request accepted!")
-      $("#requestCounter").html("");
       $("#refresh").click();
     }
   })
 
   $("#upload").click(function() {
+    $("#upload").prop("disabled", true);
     let avatar_url = $("#url").val() != "" ? $("#url").val() : 'img/avatar.png'
     $.get(avatar_url)
     .done(async function() {
       if ($("#fname").val() == "" || $("#lname").val() == "" || $("#pass").val() == "" || $("#pass").val() != $("#confirmpass").val()) {
+        $("#upload").prop("disabled", false);
         alert("Error processing registration. Forms might be incomplete or passwords might not match.")
       }
       else if (await matchFromDB($("#email").val()) == true) {
+        $("#upload").prop("disabled", false);
         alert("That email has already been registered.")
       }
       else {
